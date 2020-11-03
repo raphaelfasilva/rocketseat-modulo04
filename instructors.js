5f68053c76540af629d4e6f992ab6c6b762a5ae8
raphaelfasilva <raphaelfasilva@gmail.com>
2020-11-02 23:20:15 -0300

    modificações 02/11/20

---
+++ instructors.js
@@ -0,0 +1,9 @@
+exports.post = function(req, res) {
+    const keys = Object.keys(req.body)
+    for (key of keys) {
+        if (req.body[key] == "") {
+            return res.send("por favor validar todos os campos")
+        }
+    }
+    return res.send(req.body)
+}
